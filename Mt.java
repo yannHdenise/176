23b622978ff914904f2a764404d61d21449c1120
yannHdenise <yannahdenise.oreo@gmail.com>
2016-12-12 20:33:59 +0800

    final changes....ish for now

--- Mt.java
+++ Mt.java
@@ -55,11 +55,17 @@ public class Mt{
 	}
 
 	public String[] stringToArray(String s){// I CONVERT NIYA ANG STRING INTO ARRAY OF STRINGS
+		// String[] x = new String[3];
 
 		List<String> myList = new ArrayList<String>(Arrays.asList(s.split("\\s+")));
+
 		String[] array = new String[myList.size()];
 		myList.toArray(array); // fill the array
+
+
+
 		return array;
+
 	}
 
 	public void printing(){// I PRINT NIYA TANAN SULOD SA WORDLIST NA ARRAY LIST
@@ -76,11 +82,19 @@ public class Mt{
 				while (j < a.possible_trans.size()) {
 					Trans b = (Trans)a.possible_trans.get(j);
 					System.out.println( "inside---------------------"+b.getName());
-					System.out.println( "inside----------------------"+b.getCount());			
+					System.out.println( "inside----------------------"+b.getCount());
+
+					
 					j++;
 				}
+
+
+
 			i++;
 		}
+
+
+
 	}
 
 
@@ -101,17 +115,25 @@ public class Mt{
 			    StringBuilder asb = new StringBuilder();
 			    String aline = abr.readLine();
 
-			    while (line != null) {			        
-			          // System.out.println(m.toWordList(m.stringToArray(line),m.stringToArray(aline)));
+			    while (line != null) {
+			        
+			          System.out.println(m.toWordList(m.stringToArray(line),m.stringToArray(aline)));
 			          // m.get(m.stringToArray(line),m.stringToArray(aline));
+
 			         line = br.readLine();
 			         aline = abr.readLine();
 			    }
-
 			} catch(Exception e){  
          e.printStackTrace();
       }
 
       m.printing();
+
    }
+
+
+
+
+
 }
+
